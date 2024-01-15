6d18a05aa871388f9bcc016efcf74d865e9dc667
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2024-01-15 12:02:36 +0100

    Update app.py

--- flask-redis-compose/app.py
+++ flask-redis-compose/app.py
@@ -16,5 +16,5 @@ def null():
     return "Nullázva"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
     print("Server runing...")
